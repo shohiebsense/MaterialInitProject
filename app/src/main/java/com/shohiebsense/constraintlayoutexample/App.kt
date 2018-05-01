a271aa2feb3212eed6832a0a0580a50b5a028f6c
shohiebsense <shohieb.the.witch@gmail.com>
2018-05-01 08:06:56 +0700

    Added Language Setting Activity

---
+++ app/src/main/java/com/shohiebsense/constraintlayoutexample/App.kt
@@ -0,0 +1,28 @@
+package com.shohiebsense.constraintlayoutexample
+
+import android.app.Application
+import android.content.Context
+import android.content.res.Configuration
+import com.shohiebsense.constraintlayoutexample.util.LocaleManager
+
+
+/**
+ * Created by Shohiebsense on 01/05/2018
+ */
+
+class App : Application() {
+
+    override fun attachBaseContext(base: Context) {
+        super.attachBaseContext(LocaleManager.setLocale(base))
+    }
+
+    override fun onConfigurationChanged(newConfig: Configuration) {
+        super.onConfigurationChanged(newConfig)
+        LocaleManager.setLocale(this)
+    }
+
+    override fun onCreate() {
+        super.onCreate()
+        LocaleManager.setLocale(this)
+    }
+}
--- app/src/main/java/com/shohiebsense/constraintlayoutexample/util/AppUtil.kt
+++ app/src/main/java/com/shohiebsense/constraintlayoutexample/util/AppUtil.kt
@@ -1,6 +1,10 @@
 package com.shohiebsense.constraintlayoutexample.util
 
+import android.app.AlarmManager
+import android.app.LauncherActivity
+import android.app.PendingIntent
 import android.content.Context
+import android.content.Intent
 import android.os.CountDownTimer
 import java.text.SimpleDateFormat
 
@@ -37,6 +41,17 @@ object AppUtil {
 
             }
         }.start()
+    }
+
+
+    fun restartApplication(context: Context) {
+        val mStartActivity = Intent(context, LauncherActivity::class.java)
+        val mPendingIntentId = 123456
+        val mPendingIntent = PendingIntent.getActivity(context, mPendingIntentId, mStartActivity,
+                PendingIntent.FLAG_CANCEL_CURRENT)
+        val mgr = context.getSystemService(Context.ALARM_SERVICE) as AlarmManager
+        mgr.set(AlarmManager.RTC, System.currentTimeMillis() + 100, mPendingIntent)
+        System.exit(0)
     }
 
 }
